56f1ed0076f2e5206537860ea85fb76a69fae268
JlovesNoodles <91066411+JlovesNoodles@users.noreply.github.com>
2024-02-06 16:02:33 +0800

    Update domain.sh

--- domain.sh
+++ domain.sh
@@ -1,87 +1,91 @@
 #!/bin/bash
 # HomeBrewByChickenNoodles
 
-mkdir -p results/googleresults
-mkdir -p results/microsoftresults
+while true; do
+    mkdir -p results/googleresults
+    mkdir -p results/microsoftresults
 
+    function domainchecker() {
+        echo " "
+        echo " "
+        echo " "
+        echo "Welcome to domain checker"
+        echo " "
+        echo "Before we start choose the result you want."
+        echo " "
+        echo "Show only the Domains with"
+        echo " "
+        echo "[G]oogle [M]icrosoft "
+        echo " "
+        read -p "Your choice (G | M): " user_choice
 
-function domainchecker() {
-	echo " "
-	echo " "
-	echo " "
-	echo "Welcome to domain checker"
-	echo " "
-	echo "Before we start choose the result you want."
-	echo " "
-	echo "Show only the Domains with"
-	echo " "
-	echo "[G]oogle [M]icrosoft "
-	echo " "
- 	read -p "Your choice (G | M): " user_choice
+        if [[ $user_choice == "G" || $user_choice == "g" ]]; then
+            echo "Checking if the Domain is using Google" | lolcat
+            while read -r domain; do
+                result=$(dig +short TXT "$domain" | grep -Ei "google")
+                if [[ -n "$result" ]]; then
+                    echo "$domain is associated with Google." >> results/googleresults/googleusingdomain.txt 
+                else
+                    echo "$domain is not associated with Google." >> results/googleresults/notgoogleusingdomain.txt 
+                fi
+            done < domain_list.txt
 
-	if [[ $user_choice == "G" || $user_choice == "g" ]]; then
-        	echo "Checking if the Domain is using Google"
-        	while read -r domain; do
-            	result=$(dig +short TXT "$domain" | grep -Ei "google")
-            	if [[ -n "$result" ]]; then
-                	echo "$domain is associated with Google." >> results/googleresults/googleusingdomain.txt
-            	else
-                	echo "$domain is not associated with Google." >> results/googleresults/notgoogleusingdomain.txt
-            	fi
-            
-        	done < domain_list.txt
-        
-        echo "printing domain using google"
-                    sleep 3
-	echo -n "Finishing please wait: " | lolcat
-	for i in {1..50}; do
-	    echo -n "#"
-	    sleep 0.1
-	done | pv -lep -s 50 > /dev/null
-	cat results/googleresults/googleusingdomain.txt 
-	echo " "
-	echo " "
-	echo "printing domain not using google"
-	cat results/googleresults/notgoogleusingdomain.txt
-        
-	rm -r results
-        
-        
-    elif [[ $user_choice == "M" || $user_choice == "m" ]]; then
-        echo "Checking if the Domain is using Microsoft"
-       while read -r domain; do
-            	result=$(dig +short TXT "$domain" | grep -Ei "microsoft")
-            	if [[ -n "$result" ]]; then
-                	echo "$domain is associated with Microsoft." >> results/microsoftresults/microsoftusingdomain.txt
-            	else
-                	echo "$domain is not associated with Microsoft." >> results/microsoftresults/notmicrosoftusingdomain.txt
-            	fi
-            
-        	done < domain_list.txt
-        	
-        	
-        echo "printing domain using Microsoft"
-        
-        sleep 3
-	echo -n "Finishing please wait: " | lolcat
-	for i in {1..50}; do
-	    echo -n "#"
-	    sleep 0.1
-	done | pv -lep -s 50 > /dev/null
-	
-	cat results/microsoftresults/microsoftusingdomain.txt 
-	echo " "
-	echo " "
-	echo "printing domain not using Microsoft"
-	cat results/microsoftresults/notmicrosoftusingdomain.txt  
- 
-	rm -r results
-   
-   
-    else
-        echo "Wrong input"
-        exit 1
-    fi
-}
+            echo "printing domain using google"  
+            sleep 3
+            echo -n "Finishing please wait: " 
+            for i in {1..50}; do
+                echo -n "#"
+                sleep 0.1
+            done | pv -lep -s 50 > /dev/null
+            cat results/googleresults/googleusingdomain.txt  | lolcat
+            echo " "
+            echo " "
+            echo "printing domain not using google"
+            cat results/googleresults/notgoogleusingdomain.txt | lolcat
 
+            rm -r results
+
+        elif [[ $user_choice == "M" || $user_choice == "m" ]]; then
+            echo "Checking if the Domain is using Microsoft" 
+            while read -r domain; do
+                result=$(dig +short TXT "$domain" | grep -Ei "microsoft")
+                if [[ -n "$result" ]]; then 
+                    echo "$domain is associated with Microsoft." >> results/microsoftresults/microsoftusingdomain.txt | lolcat
+                else
+                    echo "$domain is not associated with Microsoft." >> results/microsoftresults/notmicrosoftusingdomain.txt | lolcat
+                fi
+            done < domain_list.txt
+
+            echo "printing domain using Microsoft"
+            sleep 3
+            echo -n "Finishing please wait: "
+            for i in {1..50}; do
+                echo -n "#"
+                sleep 0.1
+            done | pv -lep -s 50 > /dev/null
+
+            cat results/microsoftresults/microsoftusingdomain.txt 
+            echo " "
+            echo " "
+            echo "printing domain not using Microsoft"
+            cat results/microsoftresults/notmicrosoftusingdomain.txt  
+
+            rm -r results
+
+        else
+            echo "Wrong input"
+            exit 1
+        fi
+    }
 domainchecker
+    read -p "Do you want to check another set of domains? (Y/N): " continue_checking | lolcat
+
+    if [[ $continue_checking != "Y" || $continue_checking != "y" ]]; then
+        echo "Adios Amigo!" | lolcat
+        break
+    fi
+done
+
+
+
+
