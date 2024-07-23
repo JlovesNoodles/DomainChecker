6e82d26d5f78e24c84f3049df6f97ce267d415ba
JlovesNoodles <91066411+JlovesNoodles@users.noreply.github.com>
2024-07-23 10:36:59 +0800

    Update domain.sh

--- domain.sh
+++ domain.sh
@@ -9,27 +9,32 @@ while true; do
         echo " "
         echo " "
         echo " "
-        echo "Welcome to domain checker"
-        echo " "
-        echo "Before we start choose the result you want."
-        echo " "
-        echo "Show only the Domains with"
-        echo " "
-        echo "[G]oogle [M]icrosoft "
+        echo " ----------------------------------------------------------------"
+        echo "                   Welcome to domain checker                     "
+        echo "              Before we start choose the result you want.        "
+        echo "                  Show only the Domains with                     "
+        echo "                      [G]oogle [M]icrosoft                       " 
+        echo " ----------------------------------------------------------------"
         echo " "
         read -p "Your choice (G | M): " user_choice
 
         if [[ $user_choice == "G" || $user_choice == "g" ]]; then
-            echo "Checking if the Domain is using Google" | lolcat
+            echo "Checking if the Domain is using Google" 
             while read -r domain; do
-                result=$(dig +short TXT "$domain" | grep -Ei "google")
+                result=$(dig +short TXT "$domain" | grep -Ei "google.com")
                 if [[ -n "$result" ]]; then
-                    echo "$domain is associated with Google." >> results/googleresults/googleusingdomain.txt 
+                    echo " "
+                    echo "$domain is associated with Google." >> results/googleresults/googleusingdomain.txt
+                    echo " "
                 else
+                    echo " "
                     echo "$domain is not associated with Google." >> results/googleresults/notgoogleusingdomain.txt 
+                    echo " "
                 fi
             done < domain_list.txt
 
+            echo " "
+            echo " "
             echo "printing domain using google"  
             sleep 3
             echo -n "Finishing please wait: " 
@@ -37,22 +42,26 @@ while true; do
                 echo -n "#"
                 sleep 0.1
             done | pv -lep -s 50 > /dev/null
-            cat results/googleresults/googleusingdomain.txt  | lolcat
+            cat results/googleresults/googleusingdomain.txt  
             echo " "
             echo " "
             echo "printing domain not using google"
-            cat results/googleresults/notgoogleusingdomain.txt | lolcat
+            cat results/googleresults/notgoogleusingdomain.txt 
 
             rm -r results
 
         elif [[ $user_choice == "M" || $user_choice == "m" ]]; then
             echo "Checking if the Domain is using Microsoft" 
             while read -r domain; do
-                result=$(dig +short TXT "$domain" | grep -Ei "microsoft")
+                result=$(dig +short TXT "$domain" | grep -Ei "outlook")
                 if [[ -n "$result" ]]; then 
-                    echo "$domain is associated with Microsoft." >> results/microsoftresults/microsoftusingdomain.txt | lolcat
+                    echo " "
+                    echo "$domain is associated with Microsoft." >> results/microsoftresults/microsoftusingdomain.txt 
+                    echo " "
                 else
-                    echo "$domain is not associated with Microsoft." >> results/microsoftresults/notmicrosoftusingdomain.txt | lolcat
+                    echo " "
+                    echo "$domain is not associated with Microsoft." >> results/microsoftresults/notmicrosoftusingdomain.txt 
+                    echo " "
                 fi
             done < domain_list.txt
 
@@ -73,15 +82,20 @@ while true; do
             rm -r results
 
         else
+            echo " "
             echo "Wrong input"
             exit 1
         fi
     }
+
+
 domainchecker
-    read -p "Do you want to check another set of domains? (Y/N): " continue_checking | lolcat
+    echo " "
+    echo " "
+    read -p "Do you want to check another set of domains? (Y/N): " continue_checking 
 
     if [[ $continue_checking != "Y" || $continue_checking != "y" ]]; then
-        echo "Adios Amigo!" | lolcat
+        echo "Adios Amigo!"
         break
     fi
 done
